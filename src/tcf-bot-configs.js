9387eb4a791b9f773d3b0171b0143b94733966cc
Quentin Woivré <quentin.woivre@epitech.eu>
2019-08-11 00:19:18 +0200

    init, ...

---
+++ src/tcf-bot-configs.js
@@ -0,0 +1,15 @@
+// https://dev.twitch.tv/docs/irc/
+
+const tcfConfigs = {
+  identity: {
+    // eslint-disable-line
+    username: "",
+    password: ""
+  },
+  channels: [
+    // eslint-disable-line
+    ""
+  ]
+};
+
+module.exports = tcfConfigs;
---
+++ src/tcf-bot.js
@@ -0,0 +1,19 @@
+class tcfBot {
+  constructor() {}
+
+  onConnected(
+    // eslint-disable-line
+    tcfAddress,
+    tcfPort
+  ) {}
+
+  onMessage(
+    // eslint-disable-line
+    tcfTarget,
+    tcfContext,
+    tcfMessage,
+    tcfSelf
+  ) {}
+}
+
+module.exports = tcfBot;
